8a9d28c8e0d967df60e68aa051b6c0bec5bc8dd0
Jinmo Chong <jinmo55555@gmail.com>
2017-07-24 18:29:35 -0400

    one button added

--- iosCalc/iosCalc/ViewController.swift
+++ iosCalc/iosCalc/ViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("\(digit) is touched")
+        
     }
 
 
