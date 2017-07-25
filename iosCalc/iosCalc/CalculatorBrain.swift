d410afbcd5983507b93fc581d5fc086a684b71a1
Jinmo Chong <jinmo55555@gmail.com>
2017-07-25 13:20:12 -0400

    backbone of calculatorBrain

---
+++ iosCalc/iosCalc/CalculatorBrain.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorBrain.swift
+//  iosCalc
+//
+//  Created by Jinmo on 7/25/17.
+//  Copyright © 2017 Jinmo. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    func performOperation(symbol: String) {
+        
+        
+    }
+    
+    func setOperand(_ operand: Double) {
+
+        
+    }
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+}
