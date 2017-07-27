ba199059c16fc6a394c44cd4d57531a35e0cc122
Jinmo Chong <jinmo55555@gmail.com>
2017-07-27 16:00:46 -0400

    added binary operations

--- iosCalc/iosCalc/CalculatorBrain.swift
+++ iosCalc/iosCalc/CalculatorBrain.swift
@@ -8,24 +8,85 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
+func multiply(op1: Double, op2: Double) -> Double {
+    return op1 * op2
+}
+
 struct CalculatorBrain {
     
     private var accumulator: Double?
     
-    mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation(sqrt),
+        "cos" : Operation.unaryOperation(cos),
+        "±" : Operation.unaryOperation(changeSign),
+        "×" : Operation.binaryOperation({ (op1: Double, op2: Double) -> Double in
+            return op1 * op2
             }
-        case "c":
-            accumulator = 0
-        default:
-            break
-        }
+),
+//        "+" : Operation.binaryOperation(plus),
+//        "-" : Operation.binaryOperation(minus),
+//        "÷" : Operation.binaryOperation(divide),
+//        "×" : Operation.binaryOperation(multiply),
+        "=" : Operation.equals
+    
+    ]
+    
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
         
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+                
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+                
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+                
+            case .equals:
+                performPendingBinaryOperation()
+                break
+            }
+//            accumulator = constant
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
     }
     
     mutating func setOperand(_ operand: Double) {
